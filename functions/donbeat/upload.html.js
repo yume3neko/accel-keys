033a59f7d82a42178685ea986c29f520913683da
yume3neko <80351819+yume3neko@users.noreply.github.com>
2026-09-25 01:36:36 +0900

    security(donbeat): alias protected admin HTML extension to authenticated route

--- functions/donbeat/upload.html.js
+++ functions/donbeat/upload.html.js
@@ -1,10 +1,1 @@
-import {hasAdminSession} from '../_shared/donbeat-auth.js';
-export async function onRequestGet(context){
- if(!await hasAdminSession(context.request,context.env))
-  return new Response(null,{status:302,headers:{'location':'/donbeat/admin-login.html','cache-control':'no-store'}});
- const response=await context.next();
- const headers=new Headers(response.headers);
- headers.set('cache-control','private,no-store');
- headers.set('x-content-type-options','nosniff');
- return new Response(response.body,{status:response.status,statusText:response.statusText,headers});
-}
+export {onRequestGet} from './upload.js';
